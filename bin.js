e75f54174b3b13c9cd449e76b597d46ab7037427
Josh Duff <me@JoshDuff.com>
2015-12-03 22:49:13 -0600

    Making it actually work as a cli

--- bin.js
+++ bin.js
@@ -1,12 +1,16 @@
-var argv = require('minimist')(process.argv.slice(2))
+#!/usr/bin/env node
+
+var minimist = require('minimist')
+var socket = require('socket.io-client')
+var client = require('./index')
+
+var argv = minimist(process.argv.slice(2))
 
 if (!argv.name || !argv.server) {
 	console.log('required arguments: name, and server')
 	return
 }
 
-var socket = require('socket.io-client')
-
 var io = socket(argv.server)
 
-require('./index')(io, argv.name)
+client(io, argv.name)
