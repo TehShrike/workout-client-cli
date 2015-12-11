62d39e1230f883ee1f7e9a660ee38e974e9561ee
Josh Duff <me@JoshDuff.com>
2015-12-10 21:10:01 -0600

    logging when other people do work
    - Fixed a bug where other stdin data would keep you from confirming
    - log a message when somebody else does the work, and beep at you if you haven't done it yet
    - log on connection errors

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ var Charm = require('charm')
 
 module.exports = function (io, name) {
 	var charm = Charm(process)
+	var currentWork = {}
 	var confirm = null
+
+	io.on('connect', function(err) {
+		charm.write('Connected!\n')
+	})
+	io.on('connect_error', function(err) {
+		charm.write('Connection error: ' + err.message + '\n')
+	})
+	io.on('reconnect_error', function(err) {
+		charm.write('Error reconnecting: ' + err.message + '\n')
+	})
 
 	function write(str) {
 		charm.display('bright')
 			.write(str)
 			.display('reset')
-
 	}
 
 	function reportWorkDone(work) {
@@ -22,19 +32,32 @@ module.exports = function (io, name) {
 
 	charm.on('data', function(data) {
 		var key = data.toString()
+
 		if (confirm !== null && (key === 'y' || key === 'Y')) {
 			confirm()
 			write('\nSWEET ACTION BRO!\n')
+			confirm = null
 		}
-		confirm = null
 	})
 
 	io.on('work', function(work) {
+		currentWork = work
 		confirm = function() {
 			reportWorkDone(work)
 		}
 		beeper()
 		displayWorkPrompt(write, work)
+	})
+
+	io.on('somebody did work', function(work) {
+		if (work.reps === currentWork.reps && work.workTypeName === currentWork.workTypeName) {
+			write('✓ ' + work.personName + ' did the work!\n')
+
+			if (confirm) {
+				write('Why haven\'t you?\n')
+				beeper()
+			}
+		}
 	})
 }
 
